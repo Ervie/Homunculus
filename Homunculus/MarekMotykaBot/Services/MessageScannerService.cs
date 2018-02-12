be346395dc7a26433d94e240e523a9adf3e9ca8c
Ervin <bbuchala93@gmail.com>
2018-02-12 11:45:06 +0100

    Message scanner - further generalizion of methods.

--- Homunculus/MarekMotykaBot/Services/MessageScannerService.cs
+++ Homunculus/MarekMotykaBot/Services/MessageScannerService.cs
@@ -13,205 +13,187 @@ using System.Threading.Tasks;
 
 namespace MarekMotykaBot
 {
-    public class MessageScannerService: IDiscordService
-    {
-        private readonly DiscordSocketClient _client;
+	public class MessageScannerService : IDiscordService
+	{
+		private readonly DiscordSocketClient _client;
 
-        private readonly JSONSerializerService _serializer;
+		private readonly JSONSerializerService _serializer;
 
 		private readonly DropboxService _dropbox;
 
-        private readonly List<string> _swearWordList;
+		private readonly List<string> _swearWordList;
 
-        private readonly List<string> _waifuList;
+		private readonly List<string> _waifuList;
 
-        private readonly List<string> _marekFaceWords;
+		private readonly List<string> _marekFaceWords;
 
-        private readonly List<string> _takeuchiWords;
+		private readonly List<string> _takeuchiWords;
 
-        public IConfiguration Configuration { get; set; }
+		public IConfiguration Configuration { get; set; }
 
-        public MessageScannerService(DiscordSocketClient client, JSONSerializerService serializer, DropboxService dropbox, IConfiguration configuration)
-        {
-            _client = client;
-            _serializer = serializer;
-            Configuration = configuration;
+		public MessageScannerService(DiscordSocketClient client, JSONSerializerService serializer, DropboxService dropbox, IConfiguration configuration)
+		{
+			_client = client;
+			_serializer = serializer;
+			Configuration = configuration;
 			_dropbox = dropbox;
 
-            _swearWordList = _serializer.LoadFromFile<string>("swearWords.json");
-            _marekFaceWords = _serializer.LoadFromFile<string>("marekTrigger.json");
-            _waifuList = serializer.LoadFromFile<string>("marekWaifus.json");
-            _takeuchiWords = serializer.LoadFromFile<string>("takeuchiTrigger.json");
-        }
+			_swearWordList = _serializer.LoadFromFile<string>("swearWords.json");
+			_marekFaceWords = _serializer.LoadFromFile<string>("marekTrigger.json");
+			_waifuList = serializer.LoadFromFile<string>("marekWaifus.json");
+			_takeuchiWords = serializer.LoadFromFile<string>("takeuchiTrigger.json");
+		}
 
-        public async Task ScanMessage(SocketMessage s)
-        {
-            var message = s as SocketUserMessage;
+		public async Task ScanMessage(SocketMessage s)
+		{
+			var message = s as SocketUserMessage;
 
-            if (message == null)
-                return;
+			if (message == null)
+				return;
 
-            var context = new SocketCommandContext(_client, message);
+			var context = new SocketCommandContext(_client, message);
 
-            if (!message.Author.IsBot && !message.Content.StartsWith(Configuration["prefix"]))
-            {
-                await DetectWaifus(context, message);
-                await DetectMarekFaceTriggerWords(context, message);
-                await DetectTakeuchiFaceTriggerWords(context, message);
-                await DetectMentions(context, message);
-                await DetectSwearWord(context, message);
-            }
-        }
+			if (!message.Author.IsBot && !message.Content.StartsWith(Configuration["prefix"]))
+			{
+				await DetectWaifus(context, message);
+				await AddReactionAfterTriggerWord(context, message, _marekFaceWords, "marekface");
+				await AddReactionAfterTriggerWord(context, message, _takeuchiWords, "takeuchi");
+				await DetectMentions(context, message);
+				await DetectSwearWord(context, message);
+			}
+		}
 
-        public async Task ScanUpdateMessage(Cacheable<IMessage, ulong> oldMessage, SocketMessage s, ISocketMessageChannel channel)
-        {
-            var message = s as SocketUserMessage;
+		public async Task ScanUpdateMessage(Cacheable<IMessage, ulong> oldMessage, SocketMessage s, ISocketMessageChannel channel)
+		{
+			var message = s as SocketUserMessage;
 
-            if (message == null)
-                return;
+			if (message == null)
+				return;
 
-            var context = new SocketCommandContext(_client, message);
+			var context = new SocketCommandContext(_client, message);
 
-            if (!message.Author.IsBot)
-            {
-                await RemoveMarekFaceReaction(context, message, await DetectMarekFaceTriggerWords(context, message));
-            }
-        }
+			if (!message.Author.IsBot)
+			{
+				await RemoveReactionAfterTriggerMissing(context, message, await AddReactionAfterTriggerWord(context, message, _marekFaceWords, "marekface"), "marekface");
+				await RemoveReactionAfterTriggerMissing(context, message, await AddReactionAfterTriggerWord(context, message, _takeuchiWords, "takeuchi"), "takeuchi");
+			}
+		}
 
-        private async Task DetectMentions(SocketCommandContext context, SocketUserMessage message)
-        {
-            if (message.MentionedUsers.Where(x => x.DiscordId().Equals("MarekMotykaBot#2213") || x.DiscordId().Equals("Erina#5946")).FirstOrDefault() != null ||
-                message.Tags.Any(x => x.Type.Equals(TagType.EveryoneMention) || x.Type.Equals(TagType.HereMention)))
-            {
-                DateTime today = DateTime.Now;
+		/// <summary>
+		/// Add reaction if trigger word is detected in message.
+		/// </summary>
+		/// <returns>True if reaction emote was added.</returns>
+		private async Task<bool> AddReactionAfterTriggerWord(SocketCommandContext context, SocketUserMessage message, ICollection<string> triggerWords, string emoteCode)
+		{
+			foreach (string word in triggerWords)
+			{
+				if (message.Content.ToLowerInvariant().Contains(word) && !message.Author.IsBot)
+				{
+					GuildEmote emote = context.Guild.Emotes.Where(x => x.Name.ToLower().Equals(emoteCode)).FirstOrDefault();
 
-                if (today.DayOfWeek == DayOfWeek.Saturday || today.DayOfWeek == DayOfWeek.Sunday)
-                {
-                    if (today.Hour < 12)
-                        await context.Channel.SendMessageAsync(StringConsts.Snoring);
-                    else
-                        await context.Channel.SendMessageAsync(StringConsts.Girlfriend);
-                }
-                else
-                {
-                    if (today.Hour < 7)
-                        await context.Channel.SendMessageAsync(StringConsts.Snoring);
-                    else if (today.Hour < 17)
-                        await context.Channel.SendMessageAsync(StringConsts.Job);
-                    else
-                        await context.Channel.SendMessageAsync(StringConsts.Doctor);
-                }
+					if (emote != null)
+					{
+						await message.AddReactionAsync(emote);
+					}
+					return false;
+				}
+			}
+			return true;
+		}
 
-                
-            }
-        }
+		/// <summary>
+		/// Remove reaction if trigger word is edited out.
+		/// </summary>
+		private async Task RemoveReactionAfterTriggerMissing(SocketCommandContext context, SocketUserMessage message, bool shouldRemove, string emoteCode)
+		{
+			if (!shouldRemove)
+			{
+				GuildEmote emote = context.Guild.Emotes.Where(x => x.Name.ToLower().Equals(emoteCode)).FirstOrDefault();
 
-        /// <summary>
-        /// Check for marekface trigger words in each message and add reaction.
-        /// </summary>
-        private async Task<bool> DetectMarekFaceTriggerWords(SocketCommandContext context, SocketUserMessage message)
-        {
-            foreach (string word in _marekFaceWords)
-            {
-                if (message.Content.ToLowerInvariant().Contains(word) && !message.Author.IsBot)
-                {
-                    var marekFace = context.Guild.Emotes.Where(x => x.Name.ToLower().Equals("marekface")).FirstOrDefault();
+				if (emote != null)
+				{
+					await message.RemoveReactionAsync(emote, context.Client.CurrentUser);
+				}
+			}
+		}
 
-                    if (marekFace != null)
-                    {
-                        await message.AddReactionAsync(marekFace);
-                    }
-                    return false;
-                }
-            }
-            return true;
-        }
+		private async Task DetectMentions(SocketCommandContext context, SocketUserMessage message)
+		{
+			if (message.MentionedUsers.Where(x => x.DiscordId().Equals("MarekMotykaBot#2213") || x.DiscordId().Equals("Erina#5946")).FirstOrDefault() != null ||
+				message.Tags.Any(x => x.Type.Equals(TagType.EveryoneMention) || x.Type.Equals(TagType.HereMention)))
+			{
+				DateTime today = DateTime.Now;
 
-        /// <summary>
-        /// Check for takeuchi trigger words in each message and add reaction.
-        /// </summary>
-        private async Task<bool> DetectTakeuchiFaceTriggerWords(SocketCommandContext context, SocketUserMessage message)
-        {
-            foreach (string word in _takeuchiWords)
-            {
-                if (message.Content.ToLowerInvariant().Contains(word) && !message.Author.IsBot)
-                {
-                    var takeuchiFace = context.Guild.Emotes.Where(x => x.Name.ToLower().Equals("takeuchi")).FirstOrDefault();
+				if (today.DayOfWeek == DayOfWeek.Saturday || today.DayOfWeek == DayOfWeek.Sunday)
+				{
+					if (today.Hour < 12)
+						await context.Channel.SendMessageAsync(StringConsts.Snoring);
+					else
+						await context.Channel.SendMessageAsync(StringConsts.Girlfriend);
+				}
+				else
+				{
+					if (today.Hour < 7)
+						await context.Channel.SendMessageAsync(StringConsts.Snoring);
+					else if (today.Hour < 17)
+						await context.Channel.SendMessageAsync(StringConsts.Job);
+					else
+						await context.Channel.SendMessageAsync(StringConsts.Doctor);
+				}
+			}
+		}
 
-                    if (takeuchiFace != null)
-                    {
-                        await message.AddReactionAsync(takeuchiFace);
-                    }
-                    return false;
-                }
-            }
-            return true;
-        }
+		/// <summary>
+		/// Detect waifus name in each message
+		/// </summary>
+		private async Task DetectWaifus(SocketCommandContext context, SocketUserMessage message)
+		{
+			foreach (string waifuName in _waifuList)
+			{
+				if (message.Content.ToLowerInvariant().Contains(waifuName.ToLowerInvariant()) && !message.Author.IsBot)
+				{
+					await context.Channel.SendMessageAsync(string.Format(StringConsts.MarekWaifus, waifuName));
+					break;
+				}
+			}
+		}
 
-        /// <summary>
-        /// Detect waifus name in each message
-        /// </summary>
-        private async Task DetectWaifus(SocketCommandContext context, SocketUserMessage message)
-        {
-            foreach (string waifuName in _waifuList)
-            {
-                if (message.Content.ToLowerInvariant().Contains(waifuName.ToLowerInvariant()) && !message.Author.IsBot)
-                {
-                    await context.Channel.SendMessageAsync(string.Format(StringConsts.MarekWaifus, waifuName));
-                    break;
-                }
-            }
-        }
+		/// <summary>
+		/// Check for swearword and who posted it - increment counter;
+		/// </summary>
+		private async Task DetectSwearWord(SocketCommandContext context, SocketUserMessage message)
+		{
+			foreach (string swearWord in _swearWordList)
+			{
+				if (message.Content.ToLowerInvariant().Contains(swearWord.ToLowerInvariant()) && !message.Author.IsBot)
+				{
+					var counterList = _serializer.LoadFromFile<WordCounterEntry>("wordCounter.json");
 
-        /// <summary>
-        /// Remove marekFace reaction.
-        /// </summary>
-        private async Task RemoveMarekFaceReaction(SocketCommandContext context, SocketUserMessage message, bool shouldRemove)
-        {
-            var marekFace = context.Guild.Emotes.Where(x => x.Name.ToLower().Equals("marekface")).FirstOrDefault();
+					string messageText = message.Content.ToLowerInvariant();
+					string username = context.User.DiscordId();
 
-            if (marekFace != null && shouldRemove)
-            {
-                await message.RemoveReactionAsync(marekFace, context.Client.CurrentUser);
-            }
-        }
+					while (messageText.Contains(swearWord))
+					{
+						if (counterList.Exists(x => x.DiscordUsername.Equals(username) && x.Word.Equals(swearWord)))
+						{
+							counterList.Find(x => x.DiscordUsername.Equals(username) && x.Word.Equals(swearWord)).CounterValue++;
+						}
+						else
+						{
+							counterList.Add(new WordCounterEntry(username, context.User.Username, swearWord, 1));
+						}
 
-        /// <summary>
-        /// Check for swearword and who posted it - increment counter;
-        /// </summary>
-        private async Task DetectSwearWord(SocketCommandContext context, SocketUserMessage message)
-        {
-            foreach (string swearWord in _swearWordList)
-            {
-                if (message.Content.ToLowerInvariant().Contains(swearWord.ToLowerInvariant()) && !message.Author.IsBot)
-                {
-                    var counterList = _serializer.LoadFromFile<WordCounterEntry>("wordCounter.json");
+						int firstSubstringIndex = messageText.IndexOf(swearWord);
+						messageText = (firstSubstringIndex < 0)
+										? messageText
+										: messageText.Remove(firstSubstringIndex, swearWord.Length);
+					}
 
-                    string messageText = message.Content.ToLowerInvariant();
-                    string username = context.User.DiscordId();
-
-                    while (messageText.Contains(swearWord))
-                    {
-                        if (counterList.Exists(x => x.DiscordUsername.Equals(username) && x.Word.Equals(swearWord)))
-                        {
-                            counterList.Find(x => x.DiscordUsername.Equals(username) && x.Word.Equals(swearWord)).CounterValue++;
-                        }
-                        else
-                        {
-                            counterList.Add(new WordCounterEntry(username, context.User.Username, swearWord, 1));
-                        }
-
-                        int firstSubstringIndex = messageText.IndexOf(swearWord);
-                        messageText =  (firstSubstringIndex < 0)
-                                        ? messageText
-                                        : messageText.Remove(firstSubstringIndex, swearWord.Length);
-                    }
-
-                    _serializer.SaveToFile<WordCounterEntry>("wordCounter.json", counterList);
+					_serializer.SaveToFile<WordCounterEntry>("wordCounter.json", counterList);
 
 					await _dropbox.UploadFileAsync("wordCounter.json", "wordCounter.json");
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 }
