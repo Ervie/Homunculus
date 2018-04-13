fb3627636cc9e9137152fd523328084f63318ffe
Ervin <bbuchala93@gmail.com>
2018-04-13 20:18:25 +0200

    Logging with Nlog

--- Homunculus/MarekMotykaBot/Services/LoggingService.cs
+++ Homunculus/MarekMotykaBot/Services/LoggingService.cs
@@ -2,30 +2,44 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
+using NLog;
 using System;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace MarekMotykaBot.Services
 {
-    public class LoggingService: IDiscordService
+    public class LoggingService : IDiscordService
     {
         private readonly DiscordSocketClient _discord;
         private readonly CommandService _commands;
 
         public IConfiguration Configuration { get; set; }
 
-        private string _logDirectory { get; }
-        private string _logFile => Path.Combine(_logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
+        private Logger _logger;
 
+        private string _logDirectory;
+        
         public LoggingService(IConfiguration _configuration, DiscordSocketClient discord, CommandService commands)
         {
             Configuration = _configuration;
 
-            _logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
-
             _discord = discord;
             _commands = commands;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _logDirectory = Path.Combine(AppContext.BaseDirectory, "Logs");
+
+            if (!Directory.Exists(_logDirectory))
+                Directory.CreateDirectory(_logDirectory);
+
+            _logger = LogManager.GetCurrentClassLogger();
+
+            _logger.Info("Logger init");    
 
             _discord.Log += OnLogAsync;
             _commands.Log += OnLogAsync;
@@ -33,15 +47,16 @@ namespace MarekMotykaBot.Services
 
         private Task OnLogAsync(LogMessage msg)
         {
-            if (!Directory.Exists(_logDirectory))
-                Directory.CreateDirectory(_logDirectory);
-            if (!File.Exists(_logFile))
-                File.Create(_logFile).Dispose();
+            if (msg.Exception != null)
+            {
+                _logger.Error(msg.Exception.ToString());
+            }
+            else
+            {
+                _logger.Info(msg.Message);
+            }
 
-            string logText = $"{DateTime.UtcNow.ToString("hh:mm:ss")} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}";
-            File.AppendAllText(_logFile, logText + "\n");
-
-            return Console.Out.WriteLineAsync(logText);
+            return Task.CompletedTask;
         }
     }
 }
