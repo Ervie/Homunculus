5cc15df4e50a27b2c820e6d8c5fda266f4c250d3
Ervie <bbuchala93@gmail.com>
2019-03-20 21:06:54 +0100

    Changed Thread.Sleep() to Task.Delay()

--- Homunculus/MarekMotykaBot/Services/CommandHandlingService.cs
+++ Homunculus/MarekMotykaBot/Services/CommandHandlingService.cs
@@ -1,5 +1,4 @@
-﻿using Discord;
-using Discord.Commands;
+﻿using Discord.Commands;
 using Discord.WebSocket;
 using MarekMotykaBot.DataTypes.Caches;
 using MarekMotykaBot.ExtensionsMethods;
@@ -8,102 +7,100 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MarekMotykaBot.Services
 {
-    public class CommandHandlingService : IDiscordService
-    {
-        private readonly DiscordSocketClient _discord;
-        private readonly CommandService _commands;
+	public class CommandHandlingService : IDiscordService
+	{
+		private readonly DiscordSocketClient _discord;
+		private readonly CommandService _commands;
 		private readonly JSONSerializerService _jsonSerializer;
-        private readonly MessageScannerService _scanner;
-        private readonly Random _rng;
-        private readonly IServiceProvider _provider;
+		private readonly MessageScannerService _scanner;
+		private readonly Random _rng;
+		private readonly IServiceProvider _provider;
 
-        public IConfiguration Configuration { get; set; }
-		
-        public CommandHandlingService(IConfiguration configuration, DiscordSocketClient discord, CommandService commands, MessageScannerService scanner, Random random, IServiceProvider provider, JSONSerializerService jSONSerializer)
-        {
-            _discord = discord;
-            _commands = commands;
-            _scanner = scanner;
-            _provider = provider;
+		public IConfiguration Configuration { get; set; }
+
+		public CommandHandlingService(IConfiguration configuration, DiscordSocketClient discord, CommandService commands, MessageScannerService scanner, Random random, IServiceProvider provider, JSONSerializerService jSONSerializer)
+		{
+			_discord = discord;
+			_commands = commands;
+			_scanner = scanner;
+			_provider = provider;
 			_jsonSerializer = jSONSerializer;
-            Configuration = configuration;
-            _rng = random;
+			Configuration = configuration;
+			_rng = random;
 
-            SetStartingState();
-        }
+			SetStartingState();
+		}
 
-        public CommandService Commands => _commands;
-        
+		public CommandService Commands => _commands;
 
-        private async Task OnMessageReceivedAsync(SocketMessage s)
-        {
+		private async Task OnMessageReceivedAsync(SocketMessage s)
+		{
 			if (!(s is SocketUserMessage msg)) return;
 			if (msg.Author == _discord.CurrentUser) return;
 
-            var context = new SocketCommandContext(_discord, msg);
+			var context = new SocketCommandContext(_discord, msg);
 
-            int argPos = 0;
-            if (msg.HasStringPrefix(Configuration["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
-            {
-                if (!DeclineCommand(context, msg.Content).Result)
-                {
-                    var result = await Commands.ExecuteAsync(context, argPos, _provider);
+			int argPos = 0;
+			if (msg.HasStringPrefix(Configuration["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
+			{
+				if (!DeclineCommand(context, msg.Content).Result)
+				{
+					var result = await Commands.ExecuteAsync(context, argPos, _provider);
 
-                    if (!result.IsSuccess &&
+					if (!result.IsSuccess &&
 						result.Error != CommandError.UnknownCommand &&
 						result.Error != CommandError.BadArgCount &&
 						result.Error != CommandError.ParseFailed)
-                        await context.Channel.SendMessageAsync(result.ToString());
-                }
-            }
-        }
-        
-        private void SetStartingState()
-        {
-            _discord.MessageReceived += OnMessageReceivedAsync;
-            _discord.MessageReceived += _scanner.ScanMessage;
-            _discord.MessageUpdated += _scanner.ScanUpdateMessage;
+						await context.Channel.SendMessageAsync(result.ToString());
+				}
+			}
+		}
+
+		private void SetStartingState()
+		{
+			_discord.MessageReceived += OnMessageReceivedAsync;
+			_discord.MessageReceived += _scanner.ScanMessage;
+			_discord.MessageUpdated += _scanner.ScanUpdateMessage;
 			_discord.MessageDeleted += _scanner.ScanDeletedMessage;
-        }
+		}
 
-        private async Task<bool> DeclineCommand(SocketCommandContext context, string messageContent)
-        {
-            string commandName = messageContent.Split(' ').FirstOrDefault();
-            bool commandDeclined = false;
+		private async Task<bool> DeclineCommand(SocketCommandContext context, string messageContent)
+		{
+			string commandName = messageContent.Split(' ').FirstOrDefault();
+			bool commandDeclined = false;
 
-            if (!string.IsNullOrWhiteSpace(commandName))
-            {
+			if (!string.IsNullOrWhiteSpace(commandName))
+			{
 				if (BlockCommand(context, commandName).Result)
 				{
 					return true;
 				}
 
-                string meanResponse = string.Format(StringConsts.DeclineCommand, commandName);
-                
-                int randomInt = _rng.Next(1, 100);
+				string meanResponse = string.Format(StringConsts.DeclineCommand, commandName);
 
-                // bad luck, you suck
-                if (randomInt < 6)
-                {
-                    commandDeclined = true;
+				int randomInt = _rng.Next(1, 100);
+
+				// bad luck, you suck
+				if (randomInt < 6)
+				{
+					commandDeclined = true;
 					AddDeclineCache(context.User.DiscordId(), commandName);
-                    await context.Channel.SendMessageAsync(meanResponse);
-                }
+					await context.Channel.SendMessageAsync(meanResponse);
+				}
 				else if (randomInt == 6)
 				{
 					commandDeclined = true;
 					await context.Channel.SendMessageAsync(StringConsts.PreemptiveAttack);
-					Thread.Sleep(2000);
+					await Task.Delay(2000);
 					await context.Channel.SendMessageAsync(StringConsts.ShitString);
 				}
-            }
-            return commandDeclined;
-        }
+			}
+			return commandDeclined;
+		}
 
 		private void AddDeclineCache(string discordId, string commandName)
 		{
@@ -120,7 +117,7 @@ namespace MarekMotykaBot.Services
 		private async Task<bool> BlockCommand(SocketCommandContext context, string commandName)
 		{
 			List<DeclineCache> declineCache = _jsonSerializer.LoadFromFile<DeclineCache>("declineCache.json");
-			
+
 			if (declineCache.Count > 0 && declineCache.FirstOrDefault(x => x.DiscordUsername == context.User.DiscordId() && x.CommandName == commandName) != null)
 			{
 				string meanerResponse = string.Format(StringConsts.ToldYou, context.User.Username, commandName);
