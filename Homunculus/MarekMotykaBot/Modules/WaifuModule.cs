83b6ef1f458e555acf54e30f9b55b38dbd2105e7
Ervie <bbuchala93@gmail.com>
2019-11-18 18:24:29 +0100

    Refactored WaifuModule

--- Homunculus/MarekMotykaBot/Modules/WaifuModule.cs
+++ Homunculus/MarekMotykaBot/Modules/WaifuModule.cs
@@ -2,8 +2,6 @@
 using MarekMotykaBot.ExtensionsMethods;
 using MarekMotykaBot.Modules.Interface;
 using MarekMotykaBot.Resources;
-using MarekMotykaBot.Services;
-using MarekMotykaBot.Services.Core;
 using MarekMotykaBot.Services.Core.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -12,13 +10,11 @@ using System.Threading.Tasks;
 
 namespace MarekMotykaBot.Modules
 {
-    public class WaifuModule : ModuleBase<SocketCommandContext>, IDiscordModule
-    {
-        private readonly Random _rng;
+	public class WaifuModule : ModuleBase<SocketCommandContext>, IDiscordModule
+	{
+		private readonly Random _rng;
 
-        private readonly IJSONSerializerService _serializer;
-
-        private readonly List<string> _marekWaifuList;
+		private readonly List<string> _marekWaifuList;
 
 		public string ModuleName { get => "WaifuModule"; }
 
@@ -28,131 +24,69 @@ namespace MarekMotykaBot.Modules
 			Random random,
 			IJSONSerializerService serializer,
 			ILoggingService loggingService
-			)
-        {
-            _rng = random;
-            _serializer = serializer;
+		)
+		{
+			_rng = random;
 			LoggingService = loggingService;
 
-            _marekWaifuList = serializer.LoadFromFile<string>("marekWaifus.json");
-        }
-
-        [Command("BetterWaifu"), Alias("bw"), Summary("Waifu selector, 2 waifus")]
-        public async Task BetterWaifu(string firstWaifu, string secondWaifu)
-        {
-			var waifus = new List<string>
-			{
-				firstWaifu,
-				secondWaifu
-			};
-
-			if (waifus.Contains("Legia"))
-            {
-                await Context.Channel.SendMessageAsync(StringConsts.LegiaWarszawa);
-                return;
-            }
-
-            if (waifus.Contains("Asuna") && waifus.Contains("Erina"))
-            {
-                await Context.Channel.SendMessageAsync(StringConsts.WaifuEqual);
-                return;
-            }
-
-            foreach (string waifu in _marekWaifuList)
-            {
-				if (waifus.Contains(waifu.RemoveEmojisAndEmotes()))
-                {
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuShit, waifu));
-                    return;
-                }
-            }
-
-            int selectedWaifuIndex = _rng.Next(waifus.Count);
-
-            await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuBetter, waifus[selectedWaifuIndex]));
-
-			LoggingService.CustomCommandLog(Context.Message, ModuleName, string.Join(' ', firstWaifu, secondWaifu));
+			_marekWaifuList = serializer.LoadFromFile<string>("marekWaifus.json");
 		}
 
-        [Command("BetterWaifu"), Alias("bw"), Summary("Waifu selector, multiple waifus separated with spaces")]
-        public async Task BetterWaifu(params string[] waifus)
-        {
-            var waifusList = waifus.ToList();
-            waifusList = waifusList.Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
+		[Command("BetterWaifu"), Alias("bw"), Summary("Waifu selector, multiple waifus separated with spaces")]
+		public async Task BetterWaifu(params string[] waifus)
+		{
+			var waifusList = waifus
+				.Distinct(StringComparer.CurrentCultureIgnoreCase)
+				.ToList();
 
-            if (waifusList.Contains("Asuna") && waifusList.Contains("Erina"))
-            {
-                await Context.Channel.SendMessageAsync(StringConsts.WaifuEqual);
-                return;
-            }
-
-            foreach (string waifu in _marekWaifuList)
-            {
-                if (waifusList.Contains(waifu.RemoveEmojisAndEmotes()))
-                {
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuShit, waifu));
-                    return;
-                }
-            }
-
-            int selectedWaifuIndex = _rng.Next(waifusList.Count);
-
-            switch (waifusList.Count)
-            {
-                case 0:
-                case 1:
-                    await Context.Channel.SendMessageAsync("...");
-                    break;
-                case 2:
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuBetter, waifus[selectedWaifuIndex]));
-                    break;
-                default:
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuBest, waifusList[selectedWaifuIndex]));
-                    break;
+			if (waifusList.Contains("Asuna") && waifusList.Contains("Erina"))
+			{
+				await Context.Channel.SendMessageAsync(StringConsts.WaifuEqual);
+				return;
 			}
 
+			foreach (string waifu in _marekWaifuList)
+			{
+				if (waifusList.Contains(waifu.RemoveEmojisAndEmotes(), StringComparer.CurrentCultureIgnoreCase))
+				{
+					await Context.Channel.SendMessageAsync(string.Format(StringConsts.WaifuShit, waifu));
+					return;
+				}
+			}
+
+			int selectedWaifuIndex = _rng.Next(waifusList.Count);
+
+			string returnedAnswer = waifusList.Count switch
+			{
+				0 => "...",
+				1 => "...",
+				2 => string.Format(StringConsts.WaifuBetter, waifus[selectedWaifuIndex]),
+				_ => string.Format(StringConsts.WaifuBest, waifusList[selectedWaifuIndex])
+			};
+
+			await Context.Channel.SendMessageAsync(returnedAnswer);
 			LoggingService.CustomCommandLog(Context.Message, ModuleName, string.Join(' ', waifus));
 		}
 
-        [Command("BetterHusbando"), Alias("bh"), Summary("Husbando selector, 2 husbandos")]
-        public async Task BetterHusbando(string firstHusbando, string secondHusbando)
-        {
-			var husbandos = new List<string>
+		[Command("BetterHusbando"), Alias("bh"), Summary("Husbando selector, multiple husbandos separated with spaces")]
+		public async Task BetterHusbando(params string[] husbandos)
+		{
+			var husbandoList = husbandos
+				.Distinct(StringComparer.CurrentCultureIgnoreCase)
+				.ToList();
+
+			int selectedHusbandoIndex = _rng.Next(husbandoList.Count);
+
+			string returnedAnswer = husbandoList.Count switch
 			{
-				firstHusbando,
-				secondHusbando
+				0 => "...",
+				1 => "...",
+				2 => string.Format(StringConsts.HusbandoBetter, husbandoList[selectedHusbandoIndex]),
+				_ => string.Format(StringConsts.HusbandoBest, husbandoList[selectedHusbandoIndex])
 			};
 
-			int selectedHusbandoIndex = _rng.Next(husbandos.Count);
-
-            await Context.Channel.SendMessageAsync(string.Format(StringConsts.HusbandoBetter, husbandos[selectedHusbandoIndex]));
-
-			LoggingService.CustomCommandLog(Context.Message, ModuleName, string.Join(' ', firstHusbando, secondHusbando));
-		}
-
-        [Command("BetterHusbando"), Alias("bh"), Summary("Husbando selector, multiple husbandos separated with spaces")]
-        public async Task BetterHusbando(params string[] husbandos)
-        {
-            var husbandoList = husbandos.ToList();
-            husbandoList = husbandoList.Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
-
-            int selectedHusbandoIndex = _rng.Next(husbandoList.Count);
-
-            switch (husbandoList.Count)
-            {
-                case 0:
-                case 1:
-                    await Context.Channel.SendMessageAsync("...");
-                    break;
-                case 2:
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.HusbandoBetter, husbandoList[selectedHusbandoIndex]));
-                    break;
-                default:
-                    await Context.Channel.SendMessageAsync(string.Format(StringConsts.HusbandoBest, husbandos[selectedHusbandoIndex]));
-                    break;
-			}
-
+			await Context.Channel.SendMessageAsync(returnedAnswer);
 			LoggingService.CustomCommandLog(Context.Message, ModuleName, string.Join(' ', husbandos));
 		}
-    }
+	}
 }
