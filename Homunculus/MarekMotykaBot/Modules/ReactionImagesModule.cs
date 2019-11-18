16d31bccc5fd4188346ac84cf5c744028f79422b
Ervie <bbuchala93@gmail.com>
2019-11-18 18:40:44 +0100

    Refactored ReactionImagesModule

--- Homunculus/MarekMotykaBot/Modules/ReactionImagesModule.cs
+++ Homunculus/MarekMotykaBot/Modules/ReactionImagesModule.cs
@@ -1,26 +1,28 @@
 ﻿using Discord.Commands;
 using MarekMotykaBot.Modules.Interface;
-using MarekMotykaBot.Services;
-using MarekMotykaBot.Services.Core;
 using MarekMotykaBot.Services.Core.Interfaces;
 using System.Threading.Tasks;
 
 namespace MarekMotykaBot.Modules
 {
-    public class ReactionImagesModule : ModuleBase<SocketCommandContext>, IDiscordModule
-    {
-		private readonly string resources = System.AppContext.BaseDirectory + "/Resources/Images/Reactions/";
+	public class ReactionImagesModule : ModuleBase<SocketCommandContext>, IDiscordModule
+	{
+		private readonly string resources = $"{System.AppContext.BaseDirectory}/Resources/Images/Reactions/";
 
 		public string ModuleName { get => "ReactionImagesModule"; }
 
 		public ILoggingService LoggingService { get; }
-		
+
 		public ReactionImagesModule(ILoggingService loggingService)
 		{
 			LoggingService = loggingService;
 		}
 
 		[Command("Jameson"), Alias("jjj"), Summary("JJJ laughs at your foolishness")]
-		public async Task JJJAsync() => await Context.Channel.SendFileAsync(resources + "jjj.gif");
+		public async Task JJJAsync()
+		{
+			await Context.Channel.SendFileAsync(resources + "jjj.gif");
+			LoggingService.CustomCommandLog(Context.Message, ModuleName);
+		}
 	}
 }
