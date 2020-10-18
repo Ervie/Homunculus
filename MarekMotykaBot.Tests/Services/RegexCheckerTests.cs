bf3a1acfc20d76a1c010de50f5b49a1e1d4ef6fd
Forczu <mateusz.forczmanski@gmail.com>
2020-10-18 19:33:17 +0200

    Add reaction to what words

---
+++ MarekMotykaBot.Tests/Services/RegexCheckerTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using MarekMotykaBot.Services.Core;
+using Xunit;
+
+namespace MarekMotykaBot.Tests.Services
+{
+    public class RegexCheckerTests
+    {
+        [Theory]
+        [InlineData("co")]
+        [InlineData("COOOOOOOOO")]
+        [InlineData("Cooo")]
+        [InlineData("Cooooo????")]
+        [InlineData("Cooooo!!!???")]
+        [InlineData("Cooooo??!!")]
+        [InlineData("cccccCoooO!!!")]
+        [InlineData("....cooooo")]
+        [InlineData("....   cooooo???!")]
+        public void IsWhatWord_GivenWhatAlikeWord_ShouldReturnTrue(string message)
+        {
+            // When
+            var result = RegexChecker.IsWhatWord(message);
+            
+            // Then
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("what")]
+        [InlineData("coco")]
+        [InlineData("coooi")]
+        [InlineData("cock")]
+        [InlineData("cosiestao")]
+        [InlineData("taco")]
+        [InlineData("taco?!?!?!")]
+        [InlineData("...... co sie dzieje?")]
+        public void IsWhatWord_GivenWhatNotAlikeWord_ShouldReturnFalse(string message)
+        {
+            // When
+            var result = RegexChecker.IsWhatWord(message);
+            
+            // Then
+            result.Should().BeFalse();
+        }
+    }
+}
